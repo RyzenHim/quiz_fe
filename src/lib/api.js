3443872cc36c1bca106814332295acb10951ce01
Himanshu <himanshuupadhyay210@gmail.com>
2026-04-16 11:56:17 +0530

    deployment changes

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000",
+  baseURL:
+    process.env.NEXT_PUBLIC_API_BASE_URL ||
+    "https://quiz-be-k7hj.onrender.com" ||
+    "http://localhost:8000",
 });
 
 const responseCache = new Map();
